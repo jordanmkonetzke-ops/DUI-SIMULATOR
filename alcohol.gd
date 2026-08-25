86b377416cb2f7a341218ab395d63c453fb3c07b
jordanmkonetzke-ops <jordan.m.konetzke@gmail.com>
2026-08-24 17:32:45 -0700

    ADDED FLOAT AND ROAT aCHAHOL and hud
    made bottels of alchaol rotate and float stuck in floor thought added hud with breathalizer

---
+++ alcohol.gd
@@ -0,0 +1,27 @@
+extends Node3D
+const COCKTAIL = preload("uid://o7xhr4mclnyr")
+const GLASS_WINE = preload("uid://b717mt264uux4")
+const SODA_BOTTLE = preload("uid://dtstj15dukqcb")
+const SODA_CAN = preload("uid://pu7b0g6wnuj2")
+const SODA_GLASS = preload("uid://dq37qnb6dc3n0")
+const WINE_WHITE = preload("uid://dgg7u0e464sbv")
+const WINE_RED = preload("uid://oye1cudmo0k5")
+@onready var pivot: Node3D = $pivot
+
+var itemarray: Array[Resource]=[
+	COCKTAIL,
+	GLASS_WINE,
+	WINE_RED,
+	WINE_WHITE,
+	SODA_BOTTLE,
+	SODA_CAN,
+	SODA_GLASS
+]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var new_item = itemarray.pick_random().instantiate()
+	pivot.add_child(new_item)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
